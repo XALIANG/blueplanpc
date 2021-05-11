fe67419d4cce62b0525ecd695c1118d4fed53a58
aliang <luliang@samsino.com.cn>
2021-05-11 17:32:27 +0800

    验证码，登录校验

--- src/views/userLogin/index.vue
+++ src/views/userLogin/index.vue
@@ -6,21 +6,21 @@
         <div class="center-box">
             <a-tabs default-active-key="1" @change="callback">
                 <a-tab-pane key="1" tab="账号密码登录">
-                    <a-form-model layout="horizontal" :model="formInline" @submit="handleSubmit" @submit.native.prevent>
-                        <a-form-model-item>
-                            <a-input v-model="formInline.user" placeholder="账户：admin">
+                    <a-form-model ref="ruleLogin" layout="horizontal" :rules="rules" :model="formInline" @submit="handleSubmit" @submit.native.prevent>
+                        <a-form-model-item prop="user">
+                            <a-input v-model="formInline.user" placeholder="账户：admin" v-decorator="[
+                                 formInline.user, { rules: [{ required: true, message: 'Please input your username!' }] }, ]">
                                 <a-icon slot="prefix" type="user" style="color:rgba(0,0,0,.25)" />
                             </a-input>
                         </a-form-model-item>
-                        <a-form-model-item>
+                        <a-form-model-item prop="password">
                             <a-input v-model="formInline.password" type="password" placeholder="密码：******">
                                 <a-icon slot="prefix" type="lock" theme="twoTone" style="color:rgba(0,0,0,.25)" />
                             </a-input>
                         </a-form-model-item>
-                        <a-form-model-item>
-                            <a-input class="code-1" v-model="formInline.code" type="text " placeholder="验证码">
+                        <a-form-model-item prop="code">
+                            <a-input class="code-1" v-model="formInline.code" type="text " placeholder="验证码" @keyup.enter.native="login">
                             </a-input>
-                            <!-- <a-button size="large">获取验证码</a-button> -->
                             <span class="code-from-mid" @click="obtianCode" v-html="svg"></span>
                         </a-form-model-item>
                     </a-form-model>
@@ -51,7 +51,7 @@
             <a href="">忘记密码</a>
         </div>
         <div class="submit-login">
-            <a-button type="primary" size="large" :loading="iconLoading" @click="login">
+            <a-button type="primary" size="large" :loading="iconLoading" @click="login('ruleLogin')">
                 确定
             </a-button>
         </div>
@@ -69,11 +69,33 @@ import {
 
 export default {
     data() {
+        let validatePass = (rule, value, callback) => {
+            if (this.formInline.user === '') {
+                callback(new Error('登录名为空'));
+                this.$refs.formInline.validateField('checkPass');
+            }
+            callback();
+        };
+        let validatePass2 = (rule, value, callback) => {
+
+            if (this.formInline.user === '') {
+                callback(new Error('密码不得为空'));
+                this.$refs.formInline.validateField('checkPass');
+            }
+            callback();
+        };
+        let validatePass3 = (rule, value, callback) => {
+            if (this.formInline.code === '') {
+                callback(new Error('验证码为空'));
+                this.$refs.formInline.validateField('checkPass');
+            }
+            callback();
+        };
         return {
             svg: '',
             iconLoading: false,
             errorMsg: [],
-            serviceCode:'',
+            serviceCode: '',
             formInline: {
                 user: '',
                 password: '',
@@ -83,6 +105,23 @@ export default {
                 numberPhone: '',
                 code: ''
 
+            },
+            rules: {
+                user: [{
+                    required: true,
+                    validator: validatePass,
+                    trigger: 'change'
+                }],
+                password: [{
+                    required: true,
+                    validator: validatePass2,
+                    trigger: 'change'
+                }],
+                code: [{
+                    required: true,
+                    validator: validatePass3,
+                    trigger: 'change'
+                }],
             }
         };
 
@@ -95,29 +134,37 @@ export default {
             console.log(key);
         },
         handleSubmit(e) {
-            console.log(this.formInline);
+            console.log(e);
         },
         onChange(e) {
             console.log(`checked = ${e.target.checked}`);
         },
-        login() {
+        login(ruleLogin) {
             this.iconLoading = true;
-            this.iconLoading = !this.iconLoading;
-            this.checkForm();
-            if(this.serviceCode!=this.formInline['code']){
-                Notification['warning']({
-                    message: '验证码错误'
-                })
-                 this.formInline['code'] = '';
-                return;
-            }
-            Message.loading('正在登录中...', 1).then(() => {
-                this.$router.push('/');
-                Notification['success']({
-                    message: 'Welcome',
-                    description: `Hey, my friend`
+            this.$refs[ruleLogin].validate(valid => {
+                if (!valid) {
+                    this.iconLoading = !this.iconLoading;
+                    this.$refs[ruleLogin].resetFields()
+                    return;
+                }
+                this.checkForm();
+                if (this.serviceCode != this.formInline['code']) {
+                    Notification['warning']({
+                        message: '验证码错误'
+                    })
+                    this.iconLoading = !this.iconLoading;
+                    this.formInline['code'] = '';
+                    return;
+                }
+                Message.loading('正在登录中...', 1).then(() => {
+                    this.iconLoading = !this.iconLoading;
+                    this.$router.push('/');
+                    Notification['success']({
+                        message: 'Welcome',
+                        description: `Hey, my friend`
+                    });
                 });
-            });
+            })
 
         },
         obtianCode() {
@@ -133,18 +180,17 @@ export default {
         //类型检查 
         checkForm() {
             this.errorMsg = [];
-                if (!this.formInline['user']) {
-                    this.errorMsg.push('登录名为空')
-                }
-                if (!this.formInline['password']) {
-                    this.errorMsg.push('密码不得为空')
+            if (!this.formInline['user']) {
+                this.errorMsg.push('登录名为空')
+            }
+            if (!this.formInline['password']) {
+                this.errorMsg.push('密码不得为空')
 
-                }
-                if (!this.formInline['code']) {
-                    this.errorMsg.push('验证码为空')
-                }
-                
-         
+            }
+            if (!this.formInline['code']) {
+                this.errorMsg.push('验证码为空')
+            }
+
             console.log(this.errorMsg)
         }
     }
